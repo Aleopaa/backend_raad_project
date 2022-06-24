e385fcf125b553a016386f8a0dd95d4117403807
sigltech <101203107+sigltech@users.noreply.github.com>
2022-06-24 15:43:12 +0100

    files setup and folders

---
+++ db/seed.js
@@ -0,0 +1,23 @@
+module.exports = [
+
+]
+
+db.celebrities.drop();
+
+db.celebrities.insertMany([
+    {
+        id: 1,
+        habit: 'Drink Water',
+        comment: 'two liters a day'
+    },
+    {
+        id: 2,
+        habit: 'read book',
+        comment: '10 pages a day'
+    },
+    {
+        id: 3,
+        habit: 'go outside',
+        comment: '10 minutes a day'
+    }
+]);
---
+++ models/task.js
@@ -0,0 +1,21 @@
+const {init} = require("../initdb")
+
+module.exports = class Task {
+    constructor(data, author){
+        this.id = data.id;
+        this.habit = data.habit;
+        this.comment = data.comment;
+    };
+
+    static get all(){
+        return new Promise (async (resolve, reject) => {
+            try {
+                const taskData = await init();
+                // let tasks = taskData.rows.map(b => new Task(b));
+                resolve (tasks);
+            } catch (err) {
+                reject('Task not found');
+            }
+        });
+    };
+}
